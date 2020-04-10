b2945aac7430b4ea53e342cc3c5f161deae713f0
Yaron Tal <yaron.tal@kpn.com>
2020-04-10 10:56:07 +0200

    Added vm creation project and updated gitignore

---
+++ 1-vm/main.tf
@@ -0,0 +1,132 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+    features {}
+}
+
+# Create a resource group if it doesn’t exist
+resource "azurerm_resource_group" "terraform_workshop" {
+    name     = "terraformWorkshop1${var.user}"
+    location = "westeurope"
+
+    tags = {
+        environment = "Terraform workshop"
+    }
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = "myVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "westeurope"
+    resource_group_name = azurerm_resource_group.terraform_workshop.name
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Create subnet
+resource "azurerm_subnet" "myterraformsubnet" {
+    name                 = "mySubnet"
+    resource_group_name  = azurerm_resource_group.terraform_workshop.name
+    virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
+    address_prefix       = "10.0.1.0/24"
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "myterraformpublicip" {
+    name                         = "myPublicIP"
+    location                     = "westeurope"
+    resource_group_name          = azurerm_resource_group.terraform_workshop.name
+    allocation_method            = "Dynamic"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "myterraformnsg" {
+    name                = "myNetworkSecurityGroup"
+    location            = "westeurope"
+    resource_group_name = azurerm_resource_group.terraform_workshop.name
+    
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Create network interface
+resource "azurerm_network_interface" "myterraformnic" {
+    name                      = "myNIC"
+    location                  = "westeurope"
+    resource_group_name       = azurerm_resource_group.terraform_workshop.name
+
+    ip_configuration {
+        name                          = "myNicConfiguration"
+        subnet_id                     = azurerm_subnet.myterraformsubnet.id
+        private_ip_address_allocation = "Dynamic"
+        public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+resource "azurerm_network_interface_security_group_association" "mynicsg" {
+  network_interface_id      = azurerm_network_interface.myterraformnic.id
+  network_security_group_id = azurerm_network_security_group.myterraformnsg.id
+}
+
+# Create virtual machine
+resource "azurerm_virtual_machine" "myterraformvm" {
+    name                  = "myVM"
+    location              = "westeurope"
+    resource_group_name   = azurerm_resource_group.terraform_workshop.name
+    network_interface_ids = [azurerm_network_interface.myterraformnic.id]
+    vm_size               = "Standard_DS1_v2"
+
+    storage_os_disk {
+        name              = "myOsDisk"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Premium_LRS"
+    }
+
+    storage_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "16.04.0-LTS"
+        version   = "latest"
+    }
+
+    os_profile {
+        computer_name  = "myvm"
+        admin_username = var.user
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = true
+        ssh_keys {
+            path     = "/home/azureuser/.ssh/authorized_keys"
+            key_data = file("/home/${var.user}/.ssh/id_rsa")
+        }
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
